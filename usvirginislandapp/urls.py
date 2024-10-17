93c2a12451ead3cf1a6a4d1e5538b8c5e84990e5
rithinshibujohn <125265236+rithinshibujohn@users.noreply.github.com>
2024-10-17 17:46:25 +0530

    Rithin Fourth Commit

--- usvirginislandapp/urls.py
+++ usvirginislandapp/urls.py
@@ -21,4 +21,5 @@ from .views import *
 urlpatterns = [
     path("",index,name="index"),
       path('get_data/', sponsorpopup, name='sponsorpopup'),
+      path('getcosp_data/',cosponsorpopup,name='cosponsorpopup')
 ]
--- usvirginislandapp/views.py
+++ usvirginislandapp/views.py
@@ -29,13 +29,27 @@ def index(request):
     columns2 = [column[0] for column in cursor.description]  # Get column names for result2
     result_data2 = [dict(zip(columns2, row)) for row in result2]  # Convert the result into dictionaries
 
+     # Execute the third stored procedure (usp_BTS_PageLoad 2)
+    cursor.execute("EXEC usp_BTS_PageLoad_Member 1")
+    result3 = cursor.fetchall()
+    columns3 = [column[0] for column in cursor.description]  # Get column names for result2
+    result_data3 = [dict(zip(columns3, row)) for row in result3]  # Convert the result into dictionaries
+
+     # Execute the third stored procedure (usp_BTS_PageLoad 2)
+    cursor.execute("EXEC usp_BTS_PageLoad_Member 2")
+    result4 = cursor.fetchall()
+    columns4 = [column[0] for column in cursor.description]  # Get column names for result2
+    result_data4 = [dict(zip(columns4, row)) for row in result4]  # Convert the result into dictionaries
+
     # Close the database connection
     conn.close()
 
     # Prepare the data for the template
     data = {
         "bills": result_data1,  # Data from first stored procedure
-        "status": result_data2  # Data from second stored procedure
+        "status": result_data2,  # Data from second stored procedure
+        "member": result_data3,
+        "memberstatus": result_data4
     }
 
     # Render the template with the data from both stored procedures
@@ -52,6 +66,24 @@ def sponsorpopup(request):
         with connection.cursor() as cursor:
             # Map SpNos to the DocEntry field in your database
             cursor.execute("EXEC usp_BTS_SponList %s, %s", [SpNos, param2])
+            result = cursor.fetchall()
+            print(result)
+        # Return the result in JSON format
+        return JsonResponse({'result': result})
+    else:
+        return JsonResponse({'error': 'SpNos parameter is missing.'}, status=400)
+
+def cosponsorpopup(request):
+    # Fetch SpNos from the request (passed from HTML)
+    CoSpNos = request.GET.get('CoSpNos')
+    print('Number',CoSpNos)
+    
+    param2 = 'C'  # Static value 'C'
+    
+    if CoSpNos:  # Ensure SpNos is provided
+        with connection.cursor() as cursor:
+            # Map SpNos to the DocEntry field in your database
+            cursor.execute("EXEC usp_BTS_SponList %s, %s", [CoSpNos, param2])
             result = cursor.fetchall()
             print(result)
         # Return the result in JSON format
